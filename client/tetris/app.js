c353fc18c5fef8219680cb3dd2a36e4b2a77aeaf
Andrew <andrewmorris04@gmail.com>
2022-09-30 23:01:33 -0400

    functioning game

--- client/tetris/app.js
+++ client/tetris/app.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const theShapes = [lShape, zShape, tShape, oShape, iShape];
 
   let currentPosition = 4;
-  const currentRotation = 0;
+  let currentRotation = 0;
   let random = Math.floor(Math.random() * theShapes.length);
   console.log(random);
 
@@ -82,8 +82,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function moveLeft() {
     undraw();
-    const isALeftEdge = current.some((x) => (currentPosition + index) % width === 0);
-    if (!isAtLeftEdge) currentPosition -= 1;
+    const isALeftEdge = current.some((x) => (currentPosition + x) % width === 0);
+    if (!isALeftEdge) currentPosition -= 1;
 
     if (current.some((x) => squares[currentPosition + x].classList.contains('taken'))) {
       currentPosition += 1;
@@ -91,4 +91,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
     draw();
   }
+
+  function moveRight() {
+    undraw();
+    const isARightEdge = current.some((x) => (currentPosition + x) % width === width - 1);
+    if (!isARightEdge) currentPosition += 1;
+
+    if (current.some((x) => squares[currentPosition + x].classList.contains('taken'))) {
+      currentPosition -= 1;
+    }
+
+    draw();
+  }
+
+  function rotate() {
+    undraw();
+    // eslint-disable-next-line no-plusplus
+    currentRotation++;
+    if (currentRotation === current.length) {
+      currentRotation = 0;
+    }
+    current = theShapes[random][currentRotation];
+    draw();
+  }
+
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 38) {
+      rotate();
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 40) {
+      moveDown();
+    }
+  }
+
+  document.addEventListener('keyup', control);
 });
